af3f7ac4fd5e232e25bee5333f65b9ce2987acf6
Marco Zuniga Ugalde <mazuga92@gmail.com>
2021-04-08 03:16:05 +0000

    models sin funcionar

--- src/models.py
+++ src/models.py
@@ -1,18 +1,29 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy.orm import relationship, backref
+
+
 
 db = SQLAlchemy()
 
+planets_favorites = db.Table('planets_favorites',
+    db.Column('user_id', db.Integer, db.ForeignKey('users.id')),
+    db.Column('planet_id', db.Integer, db.ForeignKey('planets.id'))
+)
+   
+
+characters_favorites = db.Table('characters_favorites',
+    db.Column('user_id', db.Integer, db.ForeignKey('users.id')),
+    db.Column('character_id', db.Integer, db.ForeignKey('characters.id'))
+)
+
 class User(db.Model):
-    __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     first_name = db.Column(db.String(120), nullable=False)
     last_name = db.Column(db.String(120), nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
-    planets = db.relationship("Planet" , secondary= "planets_favorites")
-    characters = db.relationship("Character", secondary="characters_favorites")
+    planets = db.relationship('Planet', secondary=planets_favorites, back_populates='users')
+    characters = db.relationship('Character', secondary=characters_favorites, back_populates='users')
     
     def __repr__(self):
         return '<User %r>' % self.username
@@ -26,10 +37,9 @@ class User(db.Model):
         }
 
 class Planet(db.Model):
-    __tablename__ = 'planetas'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(120), unique=True, nullable=False)
-    users = db.relationship("User", secondary="planets_favorites")
+    users = db.relationship("User", secondary="planets_favorites", back_populates="planets")
 
 
     def __repr__(self):
@@ -42,10 +52,9 @@ class Planet(db.Model):
             # do not serialize the password, its a security breach
         }
 class Character(db.Model):
-    __tablename__ = 'characters'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(120), unique=True, nullable=False)
-    users = db.relationship("User", secondary="characters_favorites")
+    users = db.relationship("User", secondary="characters_favorites", back_populates="characters")
 
     def __repr__(self):
         return '<Character %r>' % self.name
@@ -56,38 +65,3 @@ class Character(db.Model):
             "name": self.name,
             # do not serialize the password, its a security breach
         }
-class Favorites_Planets(db.Model):
-    __tablename__ = 'planets_favorites'
-    user_id = db.Column( db.Integer , db.ForeignKey('users.id'), primary_key = True)
-    planet_id = db.Column( db.Integer , db.ForeignKey('planets.id'), primary_key = True)
-    user = db.relationship(User, backref=backref("planets_favorites", cascade="all, delete-orphan"))
-    planet = db.relationship(Planet, backref=backref("planets_favorites", cascade="all, delete-orphan"))
-    
-    def __repr__(self):
-        return '<Favorites_Planets %r>' % self.id
-
-    def serialize(self):
-        return {
-            "user_id": self.user_id,
-            "planet_id": self.planet_id,
-            # do not serialize the password, its a security breach
-        }
-
-class Favorites_Characters(db.Model): 
-    __tablename__ = 'characters_favorites'  
-    user_id = db.Column( db.Integer, db.ForeignKey('users.id'), primary_key = True)
-    character_id = db.Column( db.Integer, db.ForeignKey('characters.id'), primary_key = True)
-    user = db.relationship(User, backref=backref("characters_favorites", cascade="all, delete-orphan"))
-    character = db.relationship(Character, backref=backref("characters_favorites", cascade="all, delete-orphan"))
-    
-    def __repr__(self):
-        return '<Favorites_Characters %r>' % self.user_id
-
-    def serialize(self):
-        return {
-            "user_id": self.user_id,
-            "character_id": self.character_id,
-            # do not serialize the password, its a security breach
-        }
-    
-
